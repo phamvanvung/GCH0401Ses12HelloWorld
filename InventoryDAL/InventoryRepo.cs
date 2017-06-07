b4405ded30d886561f360f61ed2574141739c547
Pham Van Vung <phamvanvung@gmail.com>
2017-06-07 16:05:50 +0700

    Ses 12
    Ses 12 Programming in .NET

---
+++ InventoryDAL/InventoryRepo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDAL
+{
+    public class InventoryRepo
+    {
+        public IEnumerable<Inventory> GetAll() {
+            using (var context = new AutoLotEntities()) {
+                return context.Inventories.ToList<Inventory>();
+            }
+        }
+    }
+}
